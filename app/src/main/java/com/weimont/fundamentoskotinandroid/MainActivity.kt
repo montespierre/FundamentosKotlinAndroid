1bcff3bc9820a315454ce1875193ae91eb7581f6
unknown <montespierre@gmail.com>
2021-04-28 20:33:28 -0500

    activity

--- app/src/main/java/com/weimont/fundamentoskotinandroid/MainActivity.kt
+++ app/src/main/java/com/weimont/fundamentoskotinandroid/MainActivity.kt
@@ -2,7 +2,11 @@ package com.weimont.fundamentoskotinandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 // MainActivity extiende de AppCompatActivity
 class MainActivity : AppCompatActivity() {
@@ -15,10 +19,21 @@ class MainActivity : AppCompatActivity() {
         // Inflar el xml = convertir el archivo de texto xml en objetos de las clases correspondientes a las vistas (button, editText) para acceder por codigo luego
         setContentView(R.layout.activity_main)
 
-        // R aglutina a todos los recuros
-        val helloText = findViewById<TextView>(R.id.hello_text)
+        val email = findViewById<EditText>(R.id.email)
+        val sendButton = findViewById<Button>(R.id.send)
 
-        helloText.text = "Hello Android"
+        // Cuando pulsemos la vista (boton) se lanze una accion
+        sendButton.setOnClickListener {
+            // Log.d("MainActivity", "Button Clicked")
+            val message = email.text
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        }
+
+
+
+
+
+
 
 
     }
